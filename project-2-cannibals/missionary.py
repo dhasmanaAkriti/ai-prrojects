650854ef35736503af8faee610e2918eace94783
Akriti <dhasmana@union.edu>
2020-01-14 17:44:42 -0500

    updating the personal repo with project 2

---
+++ project-2-cannibals/missionary.py
@@ -0,0 +1,10 @@
+### File: missionary.py
+### Implements the missionaries and cannibals problem for state
+### space search
+
+from search import *
+
+class MissionaryState(ProblemState):
+    """
+    Implement this.
+    """
