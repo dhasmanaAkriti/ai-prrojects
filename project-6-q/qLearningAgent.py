e7b51fa9594a2d25d84525a8f941f15bf39fecb9
Akriti <dhasmana@union.edu>
2020-02-29 17:47:10 -0500

    did the first part

--- project-6-q/qLearningAgent.py
+++ project-6-q/qLearningAgent.py
@@ -1,15 +1,19 @@
 import random
 import numpy as np
 import coffeegame
+#import matplotlib as m
 
-def convert_rate_to_fraction(rate):
-    rate_string = str(rate)
-    denominator = "1"
-    list_a = rate_string.split(".")
-    numerator = list_a[0] + list_a[1]
-    for i in range(len(list_a[1])):
-        denominator += "0"
-    return (int(numerator), int(denominator))
+def printQTable(qt):
+    rows,cols  = qt.shape
+    print("State ||    LEFT  |   DOWN  |   RIGHT  | UP     |")
+    print("-------------------------------------------------")
+    for row in range(rows):
+        outstr = "   "+str(row)+"  "
+        col = qt[row]
+        for val in col:
+            outstr+='    {:6.2f}'.format(val)
+        print(outstr)
+
 
 class QLearningAgent():
     def __init__(self, max_steps, max_num_eps, environment, learning_rate = 0.1, discount_rate = 0.6, exploration_rate = 0.5):
@@ -19,53 +23,55 @@ class QLearningAgent():
         self.ms = max_steps
         self.num_eps = max_num_eps
         self.env = environment
-        self.q_table = np.zeros((self.env.state_space_size, self.env.action_space_size))
+        self.q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.rewards = []
     def learn(self):
-        num, den = convert_rate_to_fraction(self.er)
         runs = 0
         while runs < self.num_eps:
-            r = random.randint(1, den)
-            if r in range(1, num+1):
-                steps = 0
-                while steps < self.ms:
-                    action = env.action_space.sample()
-                    new_state, reward, done, info = env.step(action)
-                    # the q learning magic happens here.
-                    self.q_table[state, action] = self.q_table[state, action] * (1 - self.lr) + \
+            steps = 0
+            reward_in_ep = 0
+            print(runs)
+            state = self.env.reset()
+            while steps < self.ms:
+                r = random.random()
+                self.env.render()
+                if r < self.er:
+                    print("exploring" + str(steps))
+                    action = self.env.action_space.sample()
+                else:
+                    print("exploiting" + str(steps))
+                    action = np.argmax(self.q_table[state, :])
+                new_state, reward, done, info = env.step(action)
+                #printQTable(self.q_table)
+                # the q learning magic happens here.
+                self.q_table[state, action] = self.q_table[state, action] * (1 - self.lr) + \
                                  self.lr * (reward + self.dr * np.max(self.q_table[new_state, :]))
-                    if (done):
-                        # reset if game is over
-                        state = env.reset()
-                        steps += 1
-                    else:
-                        # otherwise update state
-                        state = new_state
-            else:
-                steps = 0
-                while steps < self.ms:
-                    action = np.arg_max(self.q_table[new_state, :])
-                    new_state, reward, done, info = env.step(action)
-                    # the q learning magic happens here.
-                    self.q_table[state, action] = self.q_table[state, action] * (1 - self.lr) + \
-                                             self.lr * (reward + self.dr * np.max(self.q_table[new_state, :]))
-                    if (done):
-                        # reset if game is over
-                        state = env.reset()
-                        steps += 1
-                    else:
-                        # otherwise update state
-                        state = new_state
-    # def behave(self):
-    #     self.learn()
-    #     state = env.reset()
-
-
-
+                reward_in_ep += reward
+                if (done):
+                    # reset if game is over
+                    break
+                else:
+                    # otherwise update state
+                    state = new_state
+                steps += 1
+            self.rewards.append(reward_in_ep)
+            runs += 1
+        print("training time finished")
+    def behave(self):
+        state = self.env.reset()
+        done = False
+        while not (done) :
+            self.env.render()
+            action = np.argmax(self.q_table[state, :])
+            new_state, reward, done, info = self.env.step(action)
+            state = new_state
+            
 
 if __name__ == "__main__":
-    env = CoffeeEnv()
-    LearningAgent = QLearningAgent(20, 10, env)
+    env = coffeegame.CoffeeEnv()
+    LearningAgent = QLearningAgent(10, 1000, env)
     LearningAgent.learn()
+    LearningAgent.behave()
 
 
 
